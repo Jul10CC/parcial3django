3d3cfb848a4cf46f6f36b35daed8a1ebe66143e8
Julio Cayax <julio.cayax@gmail.com>
2020-11-21 12:53:17 -0600

    Primer commit

---
+++ djangocrud/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Songs(model.Model):
+    title = models.CharField(max_length=32)
+    artist = models.CharField(max_length=50)
+    album = models.CharField(max_length=50)
+    release_date = models.IntegerField()
+    gender = models.CharField(max_length=30)
+    
---
+++ djangocrud/api/serializers.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from .models import Songs
+
+
+class SongsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Songs
+        fields = ['title', 'artist', 'album', 'release_date', 'gender']
